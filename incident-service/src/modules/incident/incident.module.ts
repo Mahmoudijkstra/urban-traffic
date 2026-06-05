6c5cd90867c4ca25615b7685a6a328ae4d441f1f
jihen Mansour <mansourjihen17@gmail.com>
2026-06-05 22:09:56 +0100

    authentification

--- incident-service/src/modules/incident/incident.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IncidentService } from './incident.service';
-import { IncidentResolver } from './incident.resolver';
-
-@Module({
-  providers: [IncidentService, IncidentResolver],
-})
-export class IncidentModule {}
